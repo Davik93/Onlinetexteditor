e1806d3f20a52f2d2058143b57ae5c7215e325ff
Martin Davik <davik93@hotmail.se>
2014-05-05 16:35:10 +0200

    Design fix and stuff

--- Users/Maja/Lol/Index.php
+++ Users/Maja/Lol/Index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Truck</title>
-        <link rel="stylesheet" href="css/style.css" type=".text/css.">
+        <title>Lol</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
         <script src="js/script.js"></script>
     </head>
     <body>
